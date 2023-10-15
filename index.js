584fc95ed361aefbf18024a497660f6b02640e17
Bhukya Shiva Nayak <shivanayak1863@gmail.com>
2023-10-15 06:43:02 +0000

    social button creted with react

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="title">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer education and Technology. Bhimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
